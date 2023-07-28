426a22d65c619c4d184ac96a8fefefe16355a324
Celine V <123035262+celinev-hashicorp@users.noreply.github.com>
2023-07-28 12:07:09 -0500

    added doormat

--- main.tf
+++ main.tf
@@ -1,3 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+
+    doormat = {
+      source  = "doormat.hashicorp.services/hashicorp-security/doormat"
+      version = "~> 0.0.2"
+    }
+  }
+}
+
+provider "doormat" {}
+
+data "doormat_aws_credentials" "creds" {
+  provider = doormat
+  role_arn = var.role_arn
+}
+
+variable "role_arn" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+provider "aws" {
+  access_key = data.doormat_aws_credentials.creds.access_key
+  secret_key = data.doormat_aws_credentials.creds.secret_key
+  token      = data.doormat_aws_credentials.creds.token
+  region     = var.region
+}
+
+
 data "aws_partition" "current" {}
 
 locals {
