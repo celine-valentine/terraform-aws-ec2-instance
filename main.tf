c3391b202c5a9eaf171194e54ae9ad49d9b40f4d
Celine V <123035262+celinev-hashicorp@users.noreply.github.com>
2023-07-28 12:12:22 -0500

    moved doormat to version

--- main.tf
+++ main.tf
@@ -1,17 +1,6 @@
-terraform {
-  required_providers {
-    # aws = {
-    #   source = "hashicorp/aws"
-    # }
 
-    doormat = {
-      source  = "doormat.hashicorp.services/hashicorp-security/doormat"
-      version = "~> 0.0.2"
-    }
-  }
-}
 
-provider "doormat" {}
+
 
 data "doormat_aws_credentials" "creds" {
   provider = doormat
--- versions.tf
+++ versions.tf
@@ -6,5 +6,10 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 4.66"
     }
+    doormat = {
+      source  = "doormat.hashicorp.services/hashicorp-security/doormat"
+      version = "~> 0.0.2"
+    }
   }
 }
+provider "doormat" {}
